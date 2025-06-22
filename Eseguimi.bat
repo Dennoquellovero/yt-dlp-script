eaee3b6cb546c601dd5cde7b8d87b16e2cb37b02
denno <denielkalaj@gmail.com>
2025-06-22 02:08:09 +0100

    Leggibilità

--- Eseguimi.bat
+++ Eseguimi.bat
@@ -1,7 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo Aggiorno yt-dlp.exe
+echo -------------------------- 
+yt-dlp -U
+
+echo --------------------------
 set /p answer="Vuoi scaricare anche il video? (S/N): "
+echo --------------------------
 
 set "answer=%answer:a=A%"
 set "answer=%answer:s=S%"
@@ -10,7 +16,9 @@ set "answer=%answer:n=N%"
 if "%answer%"=="S" (
 	set "command=yt-dlp -f bestvideo+bestaudio/best --cookies cookies.txt --embed-metadata --embed-thumbnail -o ".\Video\%%(title^)s.%%(ext^)s""
 
+	echo --------------------------
 	echo Sia il video che l'audio verranno scaricati
+	echo --------------------------
 
 ) else (
 
@@ -19,22 +27,25 @@ if "%answer%"=="S" (
 		set "command=yt-dlp -f bestaudio --extract-audio --embed-metadata --embed-thumbnail --convert-thumbnails jpg --ppa "ffmpeg: -c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" -o ".\Audio\%%(title^)s.%%(ext^)s" --audio-format"
 
 		echo Solo l'audio verra' scaricato.
-	
+		echo --------------------------
+		
 		set /p format="Scegli un formato audio (ES: flac, m4a, ogg): "
-	
+		echo --------------------------
+
 		set "command=!command! !format!"
 
 	) else (
+		echo --------------------------
 		echo Rispondi Si' oppure no.
+		echo --------------------------
 		exit /b
 	)
 )
 
 set /p videolink="Inserisci il collegamento al video YouTube: "
+echo --------------------------
 
 set "command=%command% "!videolink!"
-
-echo !command!
 
 start %command%
 
